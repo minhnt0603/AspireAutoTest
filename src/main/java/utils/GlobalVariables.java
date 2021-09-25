735bb703ed24e793daf55f84d74cfcca7b7f3092
minhnt0603 <minhnt0603@gmail.com>
2021-09-25 18:23:14 +0700

    commit Global variables

---
+++ src/main/java/utils/GlobalVariables.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class GlobalVariables {
+    public static String ASPIRE_URL = "https://feature-qa-automation.customer-frontend.staging.aspireapp.com/sg/";
+    public static String USERNAME = "989677728";
+    public static String PIN = "024680";
+    public static String TAB_HOME = "Home";
+    public static String TAB_MORE = "More";
+    public static String TAB_USERS_AND_ACCESS = "Users and Access";
+    public static String TAB_MY_ACCOUNTS = "My Accounts";
+}
